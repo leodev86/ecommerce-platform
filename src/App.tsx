b12181a5f7b5613cc92d93dc8a9437c987372cd2
leodev86 <leonantoniodev@gmail.com>
2026-08-06 23:45:42 -0500

    Inicializacion proyecto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-white">
+        Ecommerce funciona
+      </h1>
+    </div>
+  )
+}
+
+export default App
