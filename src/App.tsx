e2b43eedad0bceb94c56599bf3bd17638e238e2b
leodev86 <leonantoniodev@gmail.com>
2026-08-07 00:38:27 -0500

    estructura inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <h1 className="text-5xl font-bold text-white">
+    <div className="flex min-h-screen items-center justify-center bg-red-400">
+      <h1 className="text-5xl font-bold text-indigo-500">
         Ecommerce funciona
       </h1>
     </div>
